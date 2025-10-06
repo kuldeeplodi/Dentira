be1d81d5713c2840bcb9ece29e1524e121eaca29
kuldeeplodi <kuldeeplodi@gmail.com>
2025-10-06 22:41:32 +0530

    start the project

---
+++ dentira/src/app/page.tsx
@@ -0,0 +1,19 @@
+
+import { SignOutButton, SignUpButton,SignedIn,SignedOut} from "@clerk/nextjs";
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <div>
+      <h1>Home page</h1>
+      <SignedOut>
+      <SignUpButton mode="modal">Sign Up</SignUpButton>
+      </SignedOut>
+
+      <SignedIn>
+        <SignOutButton>Logout</SignOutButton>
+      </SignedIn>
+
+    </div>
+  );
+}
